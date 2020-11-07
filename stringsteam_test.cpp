6ba0039230cf838b789d1dc800292fc6948befe2
ccijunk <479148871@qq.com>
2020-11-07 19:20:19 +0800

    20201107

--- stringsteam_test.cpp
+++ stringsteam_test.cpp
@@ -1,2 +1,35 @@
 #include<iostream>
 #include<sstream>
+
+int main(){
+  using namespace std;
+
+  istringstream istr("1 5.67");
+
+  cout<<istr.str()<<endl;
+  string str = istr.str();
+  cout<<str<<endl;
+  int n ;
+  double d;
+  istr>>n;
+  istr>>d;
+
+  cout<<d<<endl;
+  cout<<n<<endl;
+  return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
